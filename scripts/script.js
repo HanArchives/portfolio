f6efbbec6ace2f97bfc5cf5ef01fdcd9ec07d494
Han Ossef <90187643+HanOssef@users.noreply.github.com>
2021-11-16 11:30:20 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+// Menu mobile device
+var deButton = document.querySelector("header nav button");
+var hetMenu = document.querySelector(".uitgeklapt_menu");
+
+deButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+    hetMenu.classList.toggle('toonMenu');
+}
+
+// Hambruger button
+var hamburgerBtn = document.querySelector('.hamburger_menu');
+var menuOpen = false;
+
+hamburgerBtn.addEventListener('click', function () {
+    if (!menuOpen) {
+        hamburgerBtn.classList.add('openMenu');
+        menuOpen = true;
+    } else {
+        hamburgerBtn.classList.remove('openMenu');
+        menuOpen = false;
+    }
+});
+
+
+
+
+
